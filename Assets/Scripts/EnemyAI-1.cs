b48863779d506c77bfb03294b4bf97d85f0e1aee
Orion Hess <OHess6369@student.pps.net>
2022-03-18 10:00:41 -0700

    Enemy Script

---
+++ Assets/Scripts/EnemyAI-1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
+
+public class Control : MonoBehaviour {
+    Transform Player;
+    [SerializeField,Range(30,90)] int FOV;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
