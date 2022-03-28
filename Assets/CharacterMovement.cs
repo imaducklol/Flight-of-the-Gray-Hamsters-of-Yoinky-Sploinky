1112aa3d8fc2b00e2f909f19be1e6ecc43402a11
Jonnathan Williams <jonnieprine7@gmail.com>
2022-03-28 11:38:15 -0700

    Animation
    Added Character animations into title screen

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+       movement.x = Input.GetAxisRaw("Horizontal");
+       movement.y = Input.GetAxisRaw("Vertical"); 
+
+       animator.SetFloat("Horizontal", movement.x);
+       animator.SetFloat("Vertical", movement.y);
+       animator.SetFloat("Speed", movement.sqrMagnitude);
+
+       spriteRenderer.flipX = movement.x < 0.01 ? true : false;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
