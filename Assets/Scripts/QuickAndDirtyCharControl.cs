5e9c1a9d3c9f67e0fcb828d95bc449241f3a1280
Orion Hess <orionhess@gmail.com>
2022-04-01 09:54:00 -0700

    Rotation controller for enemy
    Might be useful elsewhere idk

--- Assets/Scripts/QuickAndDirtyCharControl.cs
+++ Assets/Scripts/QuickAndDirtyCharControl.cs
@@ -4,23 +4,17 @@ using System.Collections;
 public class QuickAndDirtyCharControl : MonoBehaviour {
 
 	public float moveSpeed = 6;
-
-	//Rigidbody rigidbody;
 	Camera viewCamera;
 	Vector3 velocity;
+	float angle;
 
 	void Start () {
-		//rigidbody = GetComponent<Rigidbody> ();
 		viewCamera = Camera.main;
 	}
 
 	void Update () {
-		Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
-		transform.LookAt (mousePos + Vector3.up * transform.position.y);
-		velocity = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical")).normalized * moveSpeed;
-	}
-
-	void FixedUpdate() {
-		GetComponent<Rigidbody2D>().MovePosition(new Vector2(GetComponent<Rigidbody2D>().position.x, GetComponent<Rigidbody2D>().position.y) + new Vector2(velocity, velocity) * Time.fixedDeltaTime);
+		Vector3 mousePos = viewCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+		angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), 1000 * Time.deltaTime);
 	}
 }
