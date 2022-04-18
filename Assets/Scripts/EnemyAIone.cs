2e35899b17304bd0f5de787e6bb3a4fd4e1f2799
Orion Hess <orionhess@gmail.com>
2022-04-18 11:34:21 -0700

    Remove FOV stuff - start work on nav
    Tried nav mesh, does not work, starting A*
    Removed old rotation controller, no longer used.

--- Assets/Scripts/EnemyAIone.cs
+++ Assets/Scripts/EnemyAIone.cs
@@ -6,23 +6,25 @@ using UnityEngine.AI;
 using UnityEngine.Serialization;
 
 
-//         https://github.com/SebLague/Field-of-View/blob/master/Episode%2001/Scripts/FieldOfView.cs
-//         https://www.youtube.com/watch?v=G1yAkfdlDtM
 public class EnemyAIOne : MonoBehaviour {
 
-    public float Radius;
-    [Range(0,360)] public float FOV;
+    [SerializeField] private float Radius;
+    // [Range(0,360)] public float FOV;
+    [SerializeField] private LayerMask targetMask;
+    [SerializeField] private LayerMask obstacleMask;
 
-    public LayerMask targetMask;
-    public LayerMask obstacleMask;
+    [SerializeField] private List<Transform> visibleTargets = new List<Transform>();
+    private Transform target;
+    private NavMeshAgent agent;
 
-    public List<Transform> visibleTargets = new List<Transform>();
 
     void Start() {
         StartCoroutine("FindTargets", .2f);
+        agent = GetComponent<NavMeshAgent>();
     }
 
     void Update() {
+        // Color visible ones
         GameObject[] playerstomark = GameObject.FindGameObjectsWithTag("PlayerHelpers");
         foreach (var playerhelper in playerstomark) {
             if (visibleTargets.Contains(playerhelper.transform)) {
@@ -33,6 +35,17 @@ public class EnemyAIOne : MonoBehaviour {
             }
         }
 
+        // Get closest target
+        Transform closest = visibleTargets[0];
+        foreach (Transform item in visibleTargets) {
+            if (Vector2.Distance(transform.position, item.position) 
+            < Vector2.Distance(transform.position, closest.position)) {
+                closest = item;
+            }
+        }
+        // Targetting
+        // DOES NOT WORK ---- IMPLEMENT A* https://www.youtube.com/watch?v=jvtFUfJ6CP8
+        agent.SetDestination(closest.position);
     }
 
     IEnumerator FindTargets(float delay) {
@@ -51,16 +64,18 @@ public class EnemyAIOne : MonoBehaviour {
             Vector2 directionToTarget = (target.position - transform.position).normalized;
 
             float degreesToTarget = Mathf.Atan2(transform.position.y - target.transform.position.y, transform.position.x - target.transform.position.x) * Mathf.Rad2Deg;
-
+            
+            /*
             // In FOV?
             if (Mathf.DeltaAngle(transform.rotation.z + 180, Mathf.Abs(degreesToTarget)) < FOV / 2) { // NOT WORKING> FIX THIS SHIT
-                float distanceToTarget = Vector2.Distance(transform.position, target.position);
+            */
 
-                // View blocked?
-                if (!Physics2D.Raycast(transform.position, directionToTarget, distanceToTarget, obstacleMask)) {
-                    visibleTargets.Add(target);
-                }
+            float distanceToTarget = Vector2.Distance(transform.position, target.position);
+            // View blocked?
+            if (!Physics2D.Raycast(transform.position, directionToTarget, distanceToTarget, obstacleMask)) {
+                visibleTargets.Add(target);
             }
+            
         }
     }
 }
