7d277f9ca00c735dab02921a75b75b6107782e3a
Carter L <clykins8887@student.pps.net>
2022-05-04 11:10:14 -0700

    coding
    dungeon generator progress 25/100

---
+++ Assets/Scripts/RoomBank.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rooms : MonoBehaviour
+{
+    public GameObject[] middleRooms;
+    public GameObject[] leftRooms;
+    public GameObject[] rightRooms;
+    public GameObject[] spawnRooms;
+}
--- Assets/Scripts/dungeun genorator.cs
+++ Assets/Scripts/dungeun genorator.cs
@@ -4,43 +4,37 @@ using UnityEngine;
 
 public class dungeungenorator : MonoBehaviour
 {
-    private int roomTracker;
+    
+
+    private RoomBank Bank;
+    private int rando;
+    private int exitTracker;
     //remembers where the exit to the last room was (0 => left, 1 => top, 2 => right)
 
     void Start() //generate a 10 room dungeon 
     {
-        //generate starting room
-        randomInt(0,2);
-        if (0) {
-            //spawn left version of spawn room 
-            roomTracker = 0;
-        }
-        if (1) {
-            //spawn top version of spawn room
-             roomTracker = 1;
-        }
-        if (2) {
-            // spawn right version of spawn room
-             roomTracker = 2;
-        }
+        
+        Bank = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomBank>();
 
-        roomGenerator() {
-            
-            if(roomTracker == 0) {
-                randomInt(0,1);
-                if (0) {
-                    //spawn a random room with a left entrance and a left exit
-                    roomTracker = 0;
-                }
-                if (1) {
-                    //spawn a random room with a left entrance and a top exit
-                    roomTracker = 1;
-                }
-            }
-            if (roomTracker == 1) {
-                randomInt(0,2);
+        rando = Random.Range(0, Bank.spawnRooms.Length);
+        Instantiate(bank.spawnRooms[rand], transform.position, Quaternion.identity);
+        exitTracker = rando;
 
+        if(exitTracker == 0) {
+            randomInt(0,1);
+            if (0) {
+                //spawn a random room with a left entrance and a left exit
+                exitTracker = 0;
             }
+            if (1) {
+                //spawn a random room with a left entrance and a top exit
+                exitTracker = 1;
+            }
+        }
+        if (exitTracker == 1) {
+            randomInt(0,2);
+
+        
         //repeat code above for right spawn rooms but replace right with left
         //if the last spawned room was middle than get randomInt(0,2); and spawn a random room 
            // with a top entrance and a random exit
@@ -52,5 +46,5 @@ public class dungeungenorator : MonoBehaviour
          
     }
 
-
+    
 }
