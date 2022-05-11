f58a694e1b9f845eb3c02c252178890ac400265d
Jonnathan Williams <jonnieprine7@gmail.com>
2022-05-11 10:53:09 -0700

    Fixing shit

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    private float attackTime = .25f;
+    private float attackCounter = .25f;
+    private bool isAttacking;
+
+    SpriteRenderer spriteRenderer;
+    public Transform interactor;
+
+    void Start()
+    {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+       movement.x = Input.GetAxisRaw("Horizontal");
+       movement.y = Input.GetAxisRaw("Vertical"); 
+
+       animator.SetFloat("Horizontal", movement.x);
+       animator.SetFloat("Vertical", movement.y);
+       animator.SetFloat("Speed", movement.sqrMagnitude);
+
+       if(Input.GetAxisRaw("Horizontal")==1 || Input.GetAxisRaw("Vertical")== -1 || Input.GetAxisRaw("Vertical")==1 || Input.GetAxisRaw("Vertical")==-1)
+       {
+           animator.SetFloat("LastHorizontal", Input.GetAxisRaw("Horizontal"));
+           animator.SetFloat("LastVertical", Input.GetAxisRaw("Vertical"));
+       }
+      
+
+       if(isAttacking)
+       {
+            attackCounter -=Time.deltaTime;
+            if(attackCounter <= 0)
+            {
+                animator.SetBool("isAttacking", false);
+                isAttacking = false;
+            }
+       }
+
+        if (Input.GetMouseButtonDown(0)){
+            attackCounter = attackTime;
+            animator.SetBool("isAttacking", true);
+            isAttacking = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
