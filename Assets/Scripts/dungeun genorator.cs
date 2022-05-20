6e148714f42e1adcd758568887f9334da04dbcaa
Carter L <clykins8887@student.pps.net>
2022-05-20 10:02:27 -0700

    I'm stupid and dumb
    I THINK I have a way to stop the rooms from overlapping but i'm stupid and don't know how to write code

--- Assets/Scripts/dungeun genorator.cs
+++ Assets/Scripts/dungeun genorator.cs
@@ -2,62 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dungeungenorator : MonoBehaviour
+public class dungeunGenorator : MonoBehaviour
 {
     
 
     private RoomBank Bank;
     private int rando;
     private int exitRand; 
-    
+    private Vector2 spawnPos; 
     private int exitTracker; //remembers where the exit to the last room was where 0 => left, 1 => top, 2 => right
+
+    /*
+
+
+
+
+    */
+
+    /*
+        -Rooms must not overlap with eachother
+            - find the appropriate room
+            - find the transform position of the entrance spawner for that rooms prefab
+            - take the negative x value of the position then add the x position value of the exit spawner
+            - instantiate at that position
+
+
+    */
+
+    void Start() 
+    {
+        Bank = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomBank>();
+    }
+
 
     void Spawner()
     {
-        
-        Bank = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomBank>();
-
         rando = Random.Range(0, Bank.spawnRooms.Length);
         Instantiate(Bank.spawnRooms[rando], transform.position, Quaternion.identity);
         exitTracker = rando;
 
         //Last rooms exit was to the left
         if(exitTracker == 0) {
-            exitRand = Random.Range(0,1);
-            if (exitRand == 0) {
-                //spawn a random room with a left entrance and a left exit
-                exitTracker = 0;
-            }else if (exitRand == 1) {
-                //spawn a random room with a left entrance and a top exit
-                exitTracker = 1;
-            }
+            
+            
         }
 
         //last rooms exit was to the middle
         if (exitTracker == 1) {
-            exitRand = Random.Range(0,2);
-            if (exitRand == 0) {
-                //spawn a random room with a bottom entrance and a left exit
-                exitTracker = 0;
-            }else if (exitRand == 1) {
-                //spawn a random room with a bottom entrance and a top exit
-                exitTracker = 1;
-            } else if (exitRand == 2) {
-                //spawn a random room that fuffiles b-r
-                exitTracker = 2;
-            }
+            rando = RandomRange(0, Bank.middleRooms.length);
+
+            Instantiate(Bank.middleRooms[rando], transform.position, Quaternion.identity);
+            //get the exit status 
+            // exitTracker 
         }
         
         //last rooms exit was to the right
         if(exitTracker == 2) {
-            exitRand = Random.Range(0,1);
-            if (exitRand == 0) {
-                //spawn a random room with a left entrance and a left exit
-                exitTracker = 0;
-            }else if (exitRand == 1) {
-                //spawn a random room with a left entrance and a top exit
-                exitTracker = 1;
-            }
+            //
+
         }
          
     }
