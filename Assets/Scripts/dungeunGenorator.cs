adc1eb1f4c6bbb38baa65f6eccefdb71abe08891
home computer <carter.lykins@gmail.com>
2022-06-05 15:47:47 -0700

    generation work
    Generation Partly working
    > Rooms are overlapping due to the spawn point not being where the last room ended
    > Once I change the spawn point it should work

--- Assets/Scripts/dungeunGenorator.cs
+++ Assets/Scripts/dungeunGenorator.cs
@@ -9,7 +9,7 @@ public class dungeunGenorator : MonoBehaviour
     private RoomBank Bank;
     private int randoNum;
     private Vector3 spawnPos; 
-    private Vector3 entrance;
+    public Vector3 entrance;
 
     //remembers where the exit to the last room was where 0 => left, 1 => top, 2 => right
     public int exitTracker;
@@ -19,7 +19,7 @@ public class dungeunGenorator : MonoBehaviour
     /*
         //high priority
         -Rooms must not overlap with eachother
-            - find the appropriate room
+            - find the appropriate room 
             - find the transform position of the entrance spawner for that rooms prefab
                 - just instantiate and move the prefab
             - If the room spawned will be to the right or left: 
@@ -30,18 +30,40 @@ public class dungeunGenorator : MonoBehaviour
                 - take the abs
                 - add to the y-pos of the exit spawner 
                 - instantiate at that pos
-
-        //low priority
-        -Rooms must not spawn on top of eachother
-            - added a ridgidbody and box collider to the spawnpoints 
-            - on contact with anything, destroy the spawnpoint
-                - the only thing it should be contacting is the ground of the new room
-                    - if it destroys before spawning a room due to being too close to the last room, increase the spawnpoint's offset
-        -Rooms need to have an way to track where the exit is
+        - Rooms must spawn at the exit of the last room
+            - new Vector3 (-displacementX + SpawnPointX, -displacementY + SpawnPointY, 0)
+            - How do we find the spawnpointPos? 
+                - Move the spawnpoint Gameobject around? 
+                    > 
+                - New prefab attarched to all rooms that we set to the spawnpoint after the old room is created? 
+                    > might run into order issues
+                    > have to destroy the old spawnpoint
+                        > 
+        
+        (done)-Rooms need to have an way to track where the exit is
             - set each room prefab's tag to where the exit is
             - find room's tag and convert to exitTracker
 
+
     */
+
+
+    void findExit(GameObject lastRoom) 
+    {
+        if (lastRoom.tag == "exitLeft") {exitTracker = 0;}
+        if (lastRoom.tag == "exitMiddle") {exitTracker = 1;}
+        if (lastRoom.tag == "exitRight") {exitTracker = 2;}
+    }
+
+    private int xDisplacment;
+    private int yDisplacment; 
+
+
+    void exitPos(GameObject lastRoom) 
+    {
+        entrance = lastRoom.transform.Find("snapPoint").gameObject.transform.position;
+
+    }
 
     void Spawner(int roomAmount)
     {
@@ -49,27 +71,34 @@ public class dungeunGenorator : MonoBehaviour
 
         randoNum = Random.Range(0, Bank.spawnRooms.Length);
         Instantiate(Bank.spawnRooms[randoNum], transform.position, Quaternion.identity);
+        roomAmount -= 1;
+        //b/c there are only three spawnrooms i can put them in order instead of getting the tag
         exitTracker = randoNum;
+
+
         
         for (var i = 0; i < roomAmount; i++)
         {
             //left
             if(exitTracker == 0) {
                 randoNum = Random.Range(0, Bank.leftRooms.Length);
+                //Bank.leftRooms[randoNum].findG;
                 Instantiate(Bank.leftRooms[randoNum], new Vector3(0, 0, 0), Quaternion.identity);
-                
-
+                findExit(Bank.leftRooms[randoNum]);
+                exitPos(Bank.leftRooms[randoNum]);
+                roomAmount += 1; 
                 
             }
+
             //middle
             if (exitTracker == 1) {
                 randoNum = Random.Range(0, Bank.middleRooms.Length);
-                //spawnPos = 
-                Instantiate(Bank.middleRooms[randoNum], spawnPos, Quaternion.identity);
-                //get the exit status !!!
-                // exitTracker = exit status
+
+                Instantiate(Bank.middleRooms[randoNum], new Vector3(0,0,0), Quaternion.identity);
+
             }
             
+            //right
             if(exitTracker == 2) {
                 
 
